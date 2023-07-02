6585c2b8774936c175bf51b3dfdb929fc93151e3
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-07-01 20:16:28 -0500

    Script.js pseudo code added

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// When I click the button
+// The windows asks how many characters I want
+// then the window will ask if I want upper case characters
+// THEN the window will ask if I want lower case characers
+// THEN the window will ask if I want numerics.
+// THEN the window will ask if i want special characters.
+// THEN The website will generate a password into the textbank
